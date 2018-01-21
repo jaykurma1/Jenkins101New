138534b41fbbcfb3b2be1eebb09ddc0b6220d565
jaykurma1 <35468428+jaykurma1@users.noreply.github.com>
2018-01-20 23:43:41 -0600

    New Demo Build

--- Jenkins101.java
+++ Jenkins101.java
@@ -3,9 +3,8 @@ package HelloJenkins;
 public class Jenkins101 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Jenkins Integration101");
-		
+		System.out.println("Jenkins Integration101");		
 		System.out.println("Jenkins Integration104");
-		System.out.println("Jenkins Integration105");
+		
 	}
 }
